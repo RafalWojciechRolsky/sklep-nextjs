2d23958be764fe4296d1b23b91f2b5eb06e22984
RafalWojciechRolsky <raaf.em@gmail.com>
2024-02-19 12:00:24 +0100

    fixing SingleProductPage component

--- src/components/molecules/SingleProductPage.tsx
+++ src/components/molecules/SingleProductPage.tsx
@@ -1,19 +1,18 @@
-import { ProductCoverImage } from '@/components/atoms/ProductCoverImage';
-import { ProductDescription } from '@/components/atoms/ProductDescription';
-import { type Product } from '@/types/types';
+import { ProductCoverImage } from "@/components/atoms/ProductCoverImage";
+import { ProductDescription } from "@/components/atoms/ProductDescription";
+import { type ProductOnPage } from "@/types/types";
 
-export const SingleProductPage = async ({ product }: { product: Product; singlePage: boolean }) => {
-	const { name, imageSrc, type, price, description, longDescription } = product;
+export const SingleProductPage = async ({
+	product,
+}: {
+	product: ProductOnPage;
+	singlePage: boolean;
+}) => {
+	const { name, imageSrc, type, price, description } = product;
 	return (
-		<article className="mx-auto max-w-[50rem] flex flex-row gap-10 justify-center items-start p-4 mb-10">
+		<article className="mx-auto mb-10 flex max-w-[50rem] flex-row items-start justify-center gap-10 p-4">
 			<ProductCoverImage src={imageSrc} name={name} productPage={true} />
-			<ProductDescription
-				name={name}
-				type={type}
-				price={price}
-				description={description}
-				longDescription={longDescription}
-			/>
+			<ProductDescription name={name} type={type} price={price} description={description} />
 		</article>
 	);
 };
