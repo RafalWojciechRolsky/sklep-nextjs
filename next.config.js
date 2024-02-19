964b105dcbe34bc267a7bffe363d6e16af104794
RafalWojciechRolsky <raaf.em@gmail.com>
2024-02-19 14:17:36 +0100

    Next Image component

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
 		typedRoutes: true,
 		mdxRs: true,
 	},
+	images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static-ourstore.hyperfunctor.com',
+      },
+    ],
+  },
 	publicRuntimeConfig: {
 		metadataBase: 'https://sklep-nextjs-theta.vercel.app/',
 	},
--- src/components/atoms/ProductCoverImage.tsx
+++ src/components/atoms/ProductCoverImage.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
-import { type FC } from 'react';
+import { type FC } from "react";
+import NextImage from "next/image";
 
 interface IPropsTypes {
 	src: string;
@@ -10,7 +11,13 @@ interface IPropsTypes {
 export const ProductCoverImage: FC<IPropsTypes> = ({ src, name, productPage = false }) => {
 	return (
 		<figure className="overflow-hidden rounded-md duration-200 ease-out hover:scale-105">
-			<img src={src} alt={name} className={`${productPage ? 'h-96 w-80 object-cover' : ''} `} />
+			<NextImage
+				width={320}
+				height={320}
+				src={src}
+				alt={name}
+				className={`${productPage ? "h-96 w-80 object-cover" : ""} `}
+			/>
 		</figure>
 	);
 };
