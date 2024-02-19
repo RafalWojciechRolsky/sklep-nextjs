fc27de79b490e1adaaa003adbaa93a58c7b64d25
RafalWojciechRolsky <raaf.em@gmail.com>
2024-02-19 12:49:40 +0100

    rafectoring header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -11,38 +11,30 @@ export const Header: FC<IPropsTypes> = async (_props) => {
 	const graphqlResponse = await executeGraphql(CategoriesDocument, {});
 
 	const categories = graphqlResponse.categories.data;
+	const navLinks = [
+		{ href: "/", label: "Home" },
+		{ href: "/products", label: "All products" },
+		...categories.map((category) => {
+			return {
+				href: `/products/${category.slug}`,
+				label: category.name,
+			};
+		}),
+	];
 
 	return (
 		<header className="mx-auto max-w-2xl  bg-gray-100 px-4 py-6 text-gray-900 sm:px-6 lg:max-w-7xl lg:px-8">
 			<nav>
 				<ul className="flex justify-center gap-4">
-					<li>
-						<ActiveLink
-							href="/"
-							activeClassName="underline"
-							className="font-bold text-blue-400 hover:text-blue-600"
-						>
-							Home
-						</ActiveLink>
-					</li>
-					<li>
-						<ActiveLink
-							href="/products"
-							activeClassName="underline"
-							className="font-bold text-blue-400 hover:text-blue-600"
-						>
-							All products
-						</ActiveLink>
-					</li>
-					{categories.map((category) => {
+					{navLinks.map((navLink) => {
 						return (
-							<li key={category.id}>
+							<li key={navLink.href}>
 								<ActiveLink
-									href={`/products/${category.slug}`}
+									href={`${navLink.href}`}
 									activeClassName="underline"
 									className="font-bold text-blue-400 hover:text-blue-600"
 								>
-									{category.name}
+									{navLink.label}
 								</ActiveLink>
 							</li>
 						);
