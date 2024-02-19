1dfdc177025a282fcf41c9670d788307ec9834e7
RafalWojciechRolsky <raaf.em@gmail.com>
2024-02-19 10:55:40 +0100

    categories in header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,20 +1,26 @@
-import { type FC } from 'react';
-import { ActiveLink } from '@/components/atoms/ActiveLink';
+import { type FC } from "react";
+import { ActiveLink } from "@/components/atoms/ActiveLink";
+import { executeGraphql } from "@/utils/executeGraphql";
+import { CategoriesDocument } from "@/gql/graphql";
 
 interface IPropsTypes {
 	name?: string;
 }
 
-export const Header: FC<IPropsTypes> = (_props) => {
+export const Header: FC<IPropsTypes> = async (_props) => {
+	const graphqlResponse = await executeGraphql(CategoriesDocument, {});
+
+	const categories = graphqlResponse.categories.data;
+
 	return (
-		<header className="mx-auto max-w-2xl  px-4 sm:px-6 lg:max-w-7xl lg:px-8 text-gray-900 py-6 bg-gray-100">
+		<header className="mx-auto max-w-2xl  bg-gray-100 px-4 py-6 text-gray-900 sm:px-6 lg:max-w-7xl lg:px-8">
 			<nav>
 				<ul className="flex justify-center gap-4">
 					<li>
 						<ActiveLink
 							href="/"
 							activeClassName="underline"
-							className="text-blue-400 font-bold hover:text-blue-600"
+							className="font-bold text-blue-400 hover:text-blue-600"
 						>
 							Home
 						</ActiveLink>
@@ -23,11 +29,24 @@ export const Header: FC<IPropsTypes> = (_props) => {
 						<ActiveLink
 							href="/products"
 							activeClassName="underline"
-							className="text-blue-400 font-bold hover:text-blue-600"
+							className="font-bold text-blue-400 hover:text-blue-600"
 						>
-							Produkty
+							All products
 						</ActiveLink>
 					</li>
+					{categories.map((category) => {
+						return (
+							<li key={category.id}>
+								<ActiveLink
+									href={`/${category.slug}`}
+									activeClassName="underline"
+									className="font-bold text-blue-400 hover:text-blue-600"
+								>
+									{category.name}
+								</ActiveLink>
+							</li>
+						);
+					})}
 				</ul>
 			</nav>
 		</header>
