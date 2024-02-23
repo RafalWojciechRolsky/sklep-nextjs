7848a4163136180fc6105a8a9ef2c4c12dbe9c59
RafalWojciechRolsky <raaf.em@gmail.com>
2024-02-23 08:45:54 +0100

    Search page delayed

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -7,6 +7,13 @@ import { executeGraphql } from "@/utils/executeGraphql";
 const SearchPage = async ({ searchParams }: { searchParams: { query: string } }) => {
 	if (/[\.\-\\?\!\/@#\$%\^&\*\(\)=\+\[\]\{\};:'",<>\`~]/.test(searchParams.query)) {
 		redirect("/");
+	} else if (searchParams.query.length < 3 || searchParams.query.length > 20) {
+		return (
+			<section className="text-center text-xl text-slate-900">
+				<h1 className="mb-10 text-3xl font-semibold text-slate-900">Search results</h1>
+				<p>Zapytanie powinno być nie krótsze niz 3 znaki i nie dłuższe niż 20</p>
+			</section>
+		);
 	}
 
 	const graphqlResponse = await executeGraphql(ProductsSearchListDocument, {
