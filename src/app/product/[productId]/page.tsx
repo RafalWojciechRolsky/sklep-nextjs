f9b6efd53cbc773c7251d59ea4827c3c094adc7c
RafalWojciechRolsky <raaf.em@gmail.com>
2024-02-29 06:32:09 +0100

    opengraph-image

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,4 +1,4 @@
-import { type Metadata } from "next";
+// import { type Metadata } from "next";
 import { Suspense } from "react";
 import { notFound } from "next/navigation";
 import { type ProductOnPage } from "@/types/types";
